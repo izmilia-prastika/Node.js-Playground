a81160959436ce9fcaddb5f041b389c8e6d21789
ispras <9570062+izmilia-prastika@users.noreply.github.com>
2021-01-11 22:47:27 +0700

    Add files via upload

---
+++ Cara-membuat-program-nodejs-asynchronous/spawn.js
@@ -0,0 +1,16 @@
+// buat dulu child process
+const cp = require("child_process");
+
+const questionApp = cp.spawn("node", ["questions.js"]);
+
+questionApp.stdin.write("Alex \n");
+questionApp.stdin.write("Tahoe \n");
+questionApp.stdin.write("Change the world \n");
+
+questionApp.stdout.on("data", data => {
+    console.log(`from the question app: ${data}`);
+});
+
+questionApp.on("close", () => {
+    console.log(`questionApp process excited`);
+});
