bf8bd7c3336e44f1fa86364017c271a230857026
ispras <9570062+izmilia-prastika@users.noreply.github.com>
2021-02-03 18:39:09 +0700

    Program ini adalah contoh penggunaan Penulisan Fungsi Arrow dalam JS ES6.
    Program ini adalah contoh penggunaan Penulisan Fungsi Arrow dalam JS ES6.

---
+++ Ex_Files_ECMAScript_6/Exercise Files/kodingan_izmi/dasar_ec6/Writing_Arrow_Function/script.js
@@ -0,0 +1,14 @@
+//cara menggunakan arrow function 
+
+let studentList = (students) => console.log(students);
+
+studentList(["A", "B", "C"]);
+
+let list = ["apple", "banana", "strawberry"];
+list.map((item) => console.log(item));
+
+let studentScore = (students) => console.log(students);
+studentScore(["A", "A-", "B+", "B", "B-", "C", "D"]);
+
+let birds = ["budgie", "lovebird", "cockatiel", "macaw", "lorikeet", "cockatoo"];
+birds.map((item) => console.log(item));
